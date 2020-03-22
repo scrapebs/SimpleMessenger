86bcf279d4af69b08be0aa9b97b8f490198fa7be
Denis <sinkovsds@gmail.com>
2020-03-22 12:03:34 +1000

    Initial commit

---
+++ src/main/java/com/denissinkov/messenger/controller/MessageController.java
@@ -0,0 +1,61 @@
+package com.denissinkov.messenger.controller;
+
+import com.denissinkov.messenger.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private int counter = 3;
+
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{put("id", "1"); put("text", "First");}});
+        add(new HashMap<String, String>() {{put("id", "2"); put("text", "Second");}});
+        add(new HashMap<String, String>() {{put("id", "3"); put("text", "Third");}});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    @PostMapping()
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(++counter));
+        messages.add(message);
+
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDB = getMessage(id);
+        messageFromDB.putAll(message);
+
+        return messageFromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+        messages.remove(message);
+    }
+
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(message->message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+}
